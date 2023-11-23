8f8538ffb17f46f4aadb466872fad86dede88a68
striver011 <striveredu011@gmail.com>
2023-11-23 07:09:57 +0530

    SDE..🚀

--- Arrays/Next_Permutation.cpp
+++ Arrays/Next_Permutation.cpp
@@ -1,7 +1,52 @@
 class Solution {
 public:
+
+    //SIMPLE CODE FROM C++ STL
+    // void nextPermutation(vector<int>& nums) {
+    //     next_permutation(nums.begin(),nums.end());
+        
+    // }
+
+
+    class Solution {
+public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+
+        int ind=nums.size()-1;
+        
+
+        //finding the peak point...
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i-1]>=nums[i]){
+                continue;
+            }
+            else{
+                ind=i-1;
+                break;
+            }
+        }
+        
+        //valid optimization...
+        if(ind==nums.size()-1) {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        
+
+        //now time to next permutate just above that number and break...
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        
+        //sort the right half or reverse the right half...
+        sort(nums.begin()+ind+1,nums.end());
+        return;
         
     }
+};
 };
